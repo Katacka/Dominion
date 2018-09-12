d7246fba2ef1380be090569272451e0bc43c9022
Katacka <donovan21@up.edu>
2018-09-12 09:07:33 -0700

    Card back image asset added

--- app/src/main/java/project/katacka/dominion/MainActivity.java
+++ app/src/main/java/project/katacka/dominion/MainActivity.java
@@ -2,6 +2,8 @@ package project.katacka.dominion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int totalOpponentCards = 5;
+        //generateCards((TableRow) findViewById(R.id.Opponent_Cards), totalOpponentCards, R.drawable.dominion);
+
+        /*int totalOpponentCards = 5;
+        TableRow trOpponentCards =(TableRow) findViewById(R.id.Opponent_Cards);
+        for (int c=1; c<=totalOpponentCards; c++){
+            ImageView ivOpponentCard = new ImageView (this);
+            ivOpponentCard.setImageDrawable(getResources().getDrawable(R.drawable.dominion));
+            ivOpponentCard.setPadding(0, 0, 0, 0); //padding in each image if needed
+            //add here on click event etc for each image...
+            //...
+            trOpponentCards.addView(ivOpponentCard, 100,100);
+        }*/
+    }
+
+    protected void generateCards(TableRow targetRow, int totalCards, int imageID) {
+        for (int c=1; c<=totalCards; c++){
+            ImageView ivOpponentCard = new ImageView (this);
+            //ivOpponentCard.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+            ivOpponentCard.setImageDrawable(getResources().getDrawable(imageID));
+            ivOpponentCard.setPadding(0, 0, 0, 0); //padding in each image if needed
+            //add here on click event etc for each image...
+            //...
+            targetRow.addView(ivOpponentCard, 300, 300);
+        }
     }
 }
