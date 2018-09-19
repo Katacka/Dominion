09446d46bc871cae2ddd46c4ff00f80e39960c23
Katacka <donovan21@up.edu>
2018-09-19 08:02:11 -0700

    Added more explicit comments, cleaned code

--- app/src/main/java/project/katacka/dominion/MainActivity.java
+++ app/src/main/java/project/katacka/dominion/MainActivity.java
@@ -38,77 +38,94 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Player Tab code
+        //Player tab code
         //TODO: Consider writing this into a multi-purpose tab function
         String[] names = {"Smart AI", "Dumb AI", "Player 1", "Player 2"};
         setNames(names);
 
         //Populates and displays the opponent cards
-        displayCards(findViewById(R.id.Opponent_Cards), 5, new int[]{R.drawable.opponent_card});
+        displayImages(findViewById(R.id.Opponent_Cards), 5, R.drawable.opponent_card);
 
-        //Populates and displays the player cards
-        displayCards(findViewById(R.id.User_Cards), R.layout.player_card, new Cards(4));
+        //Populates the base cards (Treasure and Victory points)
+        displayCards(findViewById(R.id.Base_Cards), R.layout.shop_card, 2, new Cards());
 
         //Populates and displays the shop cards
         displayCards(findViewById(R.id.Shop_Cards), R.layout.shop_card, 5, new Cards(10));
 
-        //Populates the base cards (Treasure and Victory points)
-        displayCards(findViewById(R.id.Base_Cards), R.layout.shop_card, 2, new Cards());
+        //Populates and displays the player cards
+        displayCards(findViewById(R.id.User_Cards), R.layout.player_card, new Cards(4));
     }
 
-    //Currently used to display opponent cards
-    //TODO: Wrap this functionality into populateCard/displayCards
-    protected void displayCards(TableRow targetLayout, int totalCards, int[] imageID) {
+    //Generates and displays a row of images within a TableRow
+    protected void displayImages(TableRow targetLayout, int totalCards, int imageID) {
         for (int i = 0; i < totalCards; i++){
+            //Create, define and size image
             ImageView ivOpponentCard = new ImageView (this);
             ivOpponentCard.setScaleType(ImageView.ScaleType.FIT_XY);
-            ivOpponentCard.setImageDrawable(getResources().getDrawable(imageID[0]));
+            ivOpponentCard.setImageResource(imageID);
+
+            //Modify image parameters relating to sizing within the parent layout
             TableRow.LayoutParams trParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
             trParams.weight = 1.0f;
-            trParams.gravity = Gravity.TOP;
 
-            ivOpponentCard.setLayoutParams(trParams);
-            targetLayout.addView(ivOpponentCard);
+            //Effectively renders the image
+            targetLayout.addView(ivOpponentCard, trParams);
         }
     }
 
-    protected View populateCardLayout(View cardLayout, Card card){
+    //Generates and adds a gray filter on top of some pre-existing layout
+    protected void displayEmptyStack(View cardLayout) {
+        //Defines constraints and coloring for the gray overlay
+        LinearLayout grayOverlay = new LinearLayout(this);
+        grayOverlay.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        grayOverlay.setBackgroundColor(Color.parseColor("#c8000000"));
 
+        ((ConstraintLayout) cardLayout).addView(grayOverlay);
+    }
+
+    //
+    protected View populateCardLayout(View cardLayout, Card card){
+        //Sets the card title
         ((TextView) cardLayout.findViewById(R.id.textViewTitle))
                 .setText(card.cTitle);
 
+        //Sets the card image
         ((ImageView) cardLayout.findViewById(R.id.imageViewArt))
                 .setImageResource(card.cPhotoId);
 
+        //Sets the card text
         ((TextView) cardLayout.findViewById(R.id.textViewText))
                 .setText(card.cText);
 
+        //Sets the card cost text
         ((TextView) cardLayout.findViewById(R.id.textViewCost))
                 .setText(String.format(Locale.US, "%d", card.cCost));
 
+        //Sets the card amount text
         //TODO: Remove randomization used for example GUI
         int randomAmount = (int) (Math.random() * 11);
         ((TextView) cardLayout.findViewById(R.id.textViewAmount))
                 .setText(String.format(Locale.US, "%d", randomAmount));
 
+        //Sets the card type text
         ((TextView) cardLayout.findViewById(R.id.textViewType))
                 .setText(card.cType);
 
+        //Grays out the card layout if empty
         if (randomAmount == 0) {
-            LinearLayout grayOverlay = new LinearLayout(this);
-            grayOverlay.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT));
-            grayOverlay.setBackgroundColor(Color.parseColor("#c8000000"));
-            ((ConstraintLayout) cardLayout).addView(grayOverlay);
+            displayEmptyStack(cardLayout);
         }
 
-            return cardLayout;
+        return cardLayout;
     }
 
     protected View populatePlayerCardLayout(View cardLayout, Card card) {
+        //Reveals text for player cards, as room allows
         cardLayout.findViewById(R.id.textViewText)
                 .setVisibility(View.VISIBLE);
 
+        //Hides the card amount, as not relevant
         cardLayout.findViewById(R.id.frameLayout2)
                 .setVisibility(View.INVISIBLE);
 
@@ -148,46 +165,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Sets tab names
     protected void setNames(String[] names) {
-        if (names.length != 4) return;
-
-        ((TextView) findViewById(R.id.playerTab1).findViewById(R.id.playerName)).setText(names[0]);
-        ((TextView) findViewById(R.id.playerTab2).findViewById(R.id.playerName)).setText(names[1]);
-        ((TextView) findViewById(R.id.playerTab3).findViewById(R.id.playerName)).setText(names[2]);
-        ((TextView) findViewById(R.id.playerTab4).findViewById(R.id.playerName)).setText(names[3]);
+        ConstraintLayout tabLayout = findViewById(R.id.Player_Tabs);
+        for(int i = 0; i < names.length; i++) {
+            ((TextView) tabLayout.getChildAt(i).findViewById(R.id.playerName)).setText(names[i]);
+        }
     }
 
-    protected void setBaseCards(){
-        TableLayout baseCards = findViewById(R.id.Base_Cards);
 
-        TableRow top = (TableRow) baseCards.getChildAt(0);
-
-        View cardLayoutCopper = top.getChildAt(0);
-        Card cardDataCopper = new Card( "Copper", R.drawable.dominion_copper, "+1 Gold", 0, "TREASURE");
-        populateCardLayout(cardLayoutCopper, cardDataCopper);
-
-        View cardLayoutEstate = top.getChildAt(1);
-        Card cardDataEstate = new Card("Estate", R.drawable.dominion_estate, "1 Victory Point", 2, "VICTORY");
-        populateCardLayout(cardLayoutEstate, cardDataEstate);
-
-        TableRow mid = (TableRow) baseCards.getChildAt(1);
-
-        View cardLayoutSilver = mid.getChildAt(0);
-        Card cardDataSilver = new Card("Silver", R.drawable.dominion_silver, "+2 Gold", 3, "TREASURE");
-        populateCardLayout(cardLayoutSilver, cardDataSilver);
-
-        View cardLayoutDuchy = mid.getChildAt(1);
-        Card cardDataDuchy = new Card("Duchy", R.drawable.dominion_duchy, "3 Victory Points", 5, "VICTORY");
-        populateCardLayout(cardLayoutDuchy, cardDataDuchy);
-
-        TableRow bot = (TableRow) baseCards.getChildAt(2);
-
-        View cardLayoutGold = bot.getChildAt(0);
-        Card cardDataGold = new Card("Gold", R.drawable.dominion_gold, "+3 Gold", 6, "TREASURE");
-        populateCardLayout(cardLayoutGold, cardDataGold);
-
-        View cardLayoutProvince = bot.getChildAt(1);
-        Card cardDataProvince = new Card("Province", R.drawable.dominion_province, "6 Victory Points", 8, "VICTORY");
-        populateCardLayout(cardLayoutProvince, cardDataProvince);
-    }
 }
