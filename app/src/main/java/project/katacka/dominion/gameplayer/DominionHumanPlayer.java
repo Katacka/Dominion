560d012b0e8f7f10d54ecf7b327a3454c2457b31
Hayden Liao <liao21@up.edu>
2018-11-04 17:45:51 -0800

    whitespace tabs change in DominionHumanPlayer end turn button not updated tabActive and inActive changed to item from dimen, so they work now

--- app/src/main/java/project/katacka/dominion/gameplayer/DominionHumanPlayer.java
+++ app/src/main/java/project/katacka/dominion/gameplayer/DominionHumanPlayer.java
@@ -2,6 +2,7 @@ package project.katacka.dominion.gameplayer;
 
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -20,6 +21,7 @@ import project.katacka.dominion.gameframework.infoMsg.GameInfo;
 import project.katacka.dominion.gamestate.DominionGameState;
 
 public class DominionHumanPlayer extends GameHumanPlayer implements View.OnClickListener{
+    int startPlayer = 3;
     //textviews
     //buttons
     private DominionGameState state;
@@ -27,11 +29,12 @@ public class DominionHumanPlayer extends GameHumanPlayer implements View.OnClick
     private LinearLayout tab2 = null;
     private LinearLayout tab3 = null;
     private LinearLayout tab4 = null;
-
     ConstraintLayout tabLayout = null;
-
-    private TextView tvPlayerCard = null;
     private Button bEndTurn = null;
+    float tabInactiveVal;
+    float tabActiveVal;
+    GameMainActivity activity = null;
+
 
     public DominionHumanPlayer(String name) {
         this(name, 5); //Default starting hand size is 5
@@ -62,7 +65,6 @@ public class DominionHumanPlayer extends GameHumanPlayer implements View.OnClick
         return true;
     }
 
-
     public String toString(){
         String string = "CardView Name: " + super.name;
         return string;
@@ -70,29 +72,69 @@ public class DominionHumanPlayer extends GameHumanPlayer implements View.OnClick
 
     @Override
     public void setAsGui(GameMainActivity activity) {
-        //draw things
-
-        //tvPlayerCard = activity.findViewById(R.id.tvPlayerCard);
-
+        this.activity = activity;
         activity.setContentView(R.layout.activity_main);
         tabLayout = activity.findViewById(R.id.Player_Tabs);
-
-        tab1 = (LinearLayout) tabLayout.getChildAt(1);
-        //tab2 = (TextView) activity.findViewById(R.id.playerTab2);
-        //tab3 = (LinearLayout) tabLayout.getChildAt(3);
-        //tab4 = activity.findViewById(R.id.playerTab4);
-
-        //ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(R.dimen.tabActive, tab1.getHeight());
-        //ViewGroup.LayoutParams lp1 = tab1.getLayoutParams();
-
-        //tab1.generateLayoutParams(lp);
-
+        tab1 = (LinearLayout) tabLayout.getChildAt(0);
+        tab2 = (LinearLayout) tabLayout.getChildAt(1);
+        tab3 = (LinearLayout) tabLayout.getChildAt(2);
+        tab4 = (LinearLayout) tabLayout.getChildAt(3);
         //set default tab widths
         //set width of active player to tab active, set all others to inactive
 
+        TypedValue outValueInactive = new TypedValue();
+        //true means follow the resource if it references another resource
+        activity.getResources().getValue(R.dimen.tabInactive, outValueInactive, true);
+        tabInactiveVal = outValueInactive.getFloat();
+
+        TypedValue outValueActive = new TypedValue();
+        //true means follow the resource if it references another resource
+        activity.getResources().getValue(R.dimen.tabActive, outValueActive, true);
+        tabActiveVal = outValueActive.getFloat();
+
+        updateTabs(startPlayer);
+    }
+
+    public void updateTabs(int activePlayer){
         ConstraintSet c = new ConstraintSet();
-        c.clone((ConstraintLayout) activity.findViewById(R.id.Player_Tabs));
-        c.constrainPercentWidth(R.id.playerTab1, R.dimen.tabActive);
+        //clone Player_tabs (tabs wrapper)constraints
+        c.clone(tabLayout);
+        //set default individual tab widths as percentages of the parents constraints
+        //by default, tab1 is active
+
+        switch(activePlayer){
+            case 0:
+                c.constrainPercentWidth(R.id.playerTab1, tabActiveVal);
+                c.constrainPercentWidth(R.id.playerTab2, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab3, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab4, tabInactiveVal);
+                break;
+            case 1:
+                c.constrainPercentWidth(R.id.playerTab1, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab2, tabActiveVal);
+                c.constrainPercentWidth(R.id.playerTab3, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab4, tabInactiveVal);
+                break;
+            case 2:
+                c.constrainPercentWidth(R.id.playerTab1, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab2, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab3, tabActiveVal);
+                c.constrainPercentWidth(R.id.playerTab4, tabInactiveVal);
+                break;
+            case 3:
+                c.constrainPercentWidth(R.id.playerTab1, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab2, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab3, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab4, tabActiveVal);
+                break;
+            default:
+                c.constrainPercentWidth(R.id.playerTab1, tabActiveVal);
+                c.constrainPercentWidth(R.id.playerTab2, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab3, tabInactiveVal);
+                c.constrainPercentWidth(R.id.playerTab4, tabInactiveVal);
+                break;
+        }
+        c.applyTo(tabLayout);
     }
 
     @Override
@@ -100,7 +142,16 @@ public class DominionHumanPlayer extends GameHumanPlayer implements View.OnClick
         //get updated info
         if(info instanceof DominionGameState){
             state = (DominionGameState) info;
-            //tvPlayerCard.setText("Some other text.");
+
+            /*ConstraintSet c = new ConstraintSet();
+            //clone Player_tabs (tabs wrapper)constraints
+            c.clone(tabLayout);
+
+            c.constrainPercentWidth(R.id.playerTab1, R.dimen.tabInactive);
+            c.constrainPercentWidth(R.id.playerTab2, R.dimen.tabInactive);
+            c.constrainPercentWidth(R.id.playerTab3, R.dimen.tabInactive);
+            c.constrainPercentWidth(R.id.playerTab4, R.dimen.tabInactive);
+           */
             for(int i = 0; i< allPlayerNames.length; i++) {
                 if (state.canMove(i)) {
                     /*
@@ -149,8 +200,10 @@ public class DominionHumanPlayer extends GameHumanPlayer implements View.OnClick
      */
     protected void initAfterReady() {
         // by default, we do nothing
+
         //Sets tab names
         for(int i = 0; i < allPlayerNames.length; i++) {
+            //((TextView) tabLayout.getChildAt(i).findViewById(R.id.playerName)).setText(allPlayerNames[i]);\
             ((TextView) tabLayout.getChildAt(i).findViewById(R.id.playerName)).setText(allPlayerNames[i]);
         }
     }
